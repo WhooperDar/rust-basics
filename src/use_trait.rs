bdcaa8bb0e5dd3493f156a850e89ab52edef0e3b
whooperdars <kimdariuspanis@gmail.com>
2022-11-10 17:21:45 +0800

    trait added

--- src/use_trait.rs
+++ src/use_trait.rs
@@ -1,17 +1,36 @@
-pub fn use_trait() { 
-    let bird = Bird  {name, attack: 5}; 
-    bird.print_name(); 
 
-}
-
-// struct 
 struct Bird { 
     name: String, 
     attack: u64
 }
 
+
+
+// struct 
 impl Bird { 
     fn print_name(&self){ 
         println!("{}", self.name); 
     }
+}
+
+impl Animal for Bird  {
+    fn can_fly(&self) -> bool {
+        true
+    }
+}
+// trait -> inteface 
+trait Animal { 
+    fn can_fly(&self) -> bool; 
+    fn is_animal(&self) -> bool { 
+        true
+    }
+}
+
+pub fn use_trait() { 
+    let name = String::from("Bird"); 
+
+    let bird: Bird = Bird {name , attack: 5}; 
+    bird.print_name(); 
+
+    println!("{}", bird.can_fly()); 
 }
